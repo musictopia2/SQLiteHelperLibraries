5a49bdc966243cc0f2d4b04394bd2fd9e093176d
Andy <musictopia2@hotmail.com>
2021-11-12 11:15:46 -0700

    Add project files.

---
+++ CustomSQLiteConnectionClass.cs
@@ -0,0 +1,20 @@
+﻿namespace SQLiteHelperLibraries;
+public class CustomSQLiteConnectionClass : ISQLiteConnector
+{
+    EnumDatabaseCategory IDbConnector.GetCategory(IDbConnection connection)
+    {
+        if (connection is SQLiteConnection == false)
+        {
+            throw new CustomBasicException("Should have been a sql lite connection.  Rethink");
+        }
+        return EnumDatabaseCategory.SQLite;
+    }
+    IDbConnection IDbConnector.GetConnection(EnumDatabaseCategory category, string connectionString)
+    {
+        if (category != EnumDatabaseCategory.SQLite)
+        {
+            throw new CustomBasicException("Only sqlite is supported for this category.  Rethink");
+        }
+        return new SQLiteConnection(connectionString);
+    }
+}
---
+++ GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using CommonBasicLibraries.BasicDataSettingsAndProcesses;
+global using CommonBasicLibraries.DatabaseHelpers.MiscClasses;
+global using CommonBasicLibraries.DatabaseHelpers.MiscInterfaces;
+global using System.Data;
+global using System.Data.SQLite;
