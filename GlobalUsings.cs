ffab0b377c4223f2efb1534dfd355a8276b245f3
Andy Walter <musictopia2@hotmail.com>
2024-02-08 13:27:19 -0700

    Added function to create sqlite document databases.

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -3,3 +3,4 @@ global using CommonBasicLibraries.DatabaseHelpers.MiscClasses;
 global using CommonBasicLibraries.DatabaseHelpers.MiscInterfaces;
 global using System.Data;
 global using System.Data.SQLite;
+global using dd1 = CommonBasicLibraries.DatabaseHelpers.MiscClasses.GlobalClass;
---
+++ SqliteCreateDocumentDatabaseClass.cs
@@ -0,0 +1,19 @@
+﻿namespace SQLiteHelperLibraries;
+public static class SqliteCreateDocumentDatabaseClass
+{
+    public static void RegisterCreatingDocumentDatabase()
+    {
+        dd1.CreateSqliteDatabase = CreateSqliteDatabase;
+    }
+    private static void CreateSqliteDatabase(string path)
+    {
+        string connectionString = $"Data Source={path};Version=3;";
+        SQLiteConnection m_dbConnection = new(connectionString);
+        m_dbConnection.Open();
+        string sql;
+        SQLiteCommand command;
+        sql = "Create Table DocumentTable (ID INTEGER PRIMARY KEY AutoIncrement, DatabaseName Text, CollectionName Text, JsonData Text)";
+        command = new(sql, m_dbConnection);
+        command.ExecuteNonQuery();
+    }
+}
